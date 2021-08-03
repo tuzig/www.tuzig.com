51acc1fa638523fd99e52b07470197abbd6ee349
Benny Daon <benny@tuzig.com>
2021-08-03 15:34:06 +0300

    Cleaning up

--- static/pragrmatic_unicode/slippy/get_files.sh
+++
@@ -1,9 +0,0 @@
-wget "http://nedbatchelder.com/text/unipain/slippy/jquery.min.js"
-wget "http://nedbatchelder.com/text/unipain/slippy/jquery.history.js"
-wget "http://nedbatchelder.com/text/unipain/slippy/slippy.js"
-wget "http://nedbatchelder.com/text/unipain/slippy/slippy.css"
-wget "http://nedbatchelder.com/text/unipain/slippy/slippy-pure.css"
-wget "http://nedbatchelder.com/text/unipain/highlight/highlight.pack.js"
-wget "http://nedbatchelder.com/text/unipain/highlight/vs.css"
-wget "http://nedbatchelder.com/text/unipain/lineselect.js"
-
